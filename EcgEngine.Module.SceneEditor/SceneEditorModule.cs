f94b8659b498083461be851b1b8757e0186bfc9f
yujinshin <raki2001@o365.yonsei.ac.kr>
2020-11-16 22:25:45 +0900

    Added SceneEditorModule

---
+++ EcgEngine.Module.SceneEditor/SceneEditorModule.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+
+namespace EcgEngine.Module.SceneEditor
+{
+    public class SceneEditorModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ EcgEngine.Module.SceneEditor/Views/SceneEditorPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace EcgEngine.Module.SceneEditor.Views
+{
+    /// <summary>
+    /// Interaction logic for SceneEditorPanel
+    /// </summary>
+    public partial class SceneEditorPanel : UserControl
+    {
+        public SceneEditorPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
