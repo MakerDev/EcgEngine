673a591e6f26abf7d8d3d1830401953f21ddd35f
yujinshin <raki2001@o365.yonsei.ac.kr>
2020-11-20 18:09:36 +0900

    Added KeyCodeToRadioButtonStateConverter for choosing between KeyEventTypes

---
+++ EcgEngine.Core/Converters/KeyEventTypeToRadioButtonStateConverter.cs
@@ -0,0 +1,30 @@
+﻿using EcgEngine.Models.VisualScript.Triggers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace EcgEngine.Core.Converters
+{
+    public class KeyEventTypeToRadioButtonStateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return null;
+
+            KeyEventType keyEventType = (KeyEventType)value;
+            int paramInt = int.Parse(parameter.ToString());
+            return ((int)keyEventType == paramInt);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return null;
+
+            return Enum.Parse(typeof(KeyEventType), parameter.ToString());
+        }
+    }
+}
