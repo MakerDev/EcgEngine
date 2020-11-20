e6218906befa8361ccf4c52733306419832a59ff
yujinshin <raki2001@o365.yonsei.ac.kr>
2020-11-20 16:18:46 +0900

    KeyEventTrigger json parsing part is moved to KeyEventTrigger class

--- Runtime/KeyEventTrigger.cpp
+++ Runtime/KeyEventTrigger.cpp
@@ -1,5 +1,15 @@
 #include "KeyEventTrigger.h"
 
+using namespace std;
+
+unique_ptr<KeyEventTrigger> KeyEventTrigger::CreateFromJsonValue(const rapidjson::Value& keyEventTrigger)
+{
+	KeyEventType keyEventType = static_cast<KeyEventType>(keyEventTrigger["KeyEventType"].GetInt());
+	EventKeyboard::KeyCode keycode = static_cast<EventKeyboard::KeyCode>(keyEventTrigger["Key"].GetInt());
+
+	return make_unique<KeyEventTrigger>(keyEventType, keycode);
+}
+
 KeyEventTrigger::KeyEventTrigger(KeyEventType keyEventType, EventKeyboard::KeyCode keycode) noexcept
 	: _type(keyEventType), _keyCode(keycode)
 {
--- Runtime/KeyEventTrigger.h
+++ Runtime/KeyEventTrigger.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <rapidjson/document.h>
 
 #include "Trigger.h"
 #include "KeyEventTypes.h"
@@ -9,10 +10,11 @@ using namespace cocos2d;
 class KeyEventTrigger : public Trigger
 {
 public:
+	static std::unique_ptr<KeyEventTrigger> CreateFromJsonValue(const rapidjson::Value& keyEventTrigger);
 	KeyEventTrigger(KeyEventType keyEventType, EventKeyboard::KeyCode keycode) noexcept;
 
 	bool IsMatched(const EventContext& context) const;
-	
+
 private:
 	KeyEventType _type;
 	EventKeyboard::KeyCode _keyCode;
