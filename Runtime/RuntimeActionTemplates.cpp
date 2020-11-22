8d42607253afd942614e36cd456a692a5c889f65
yujinshin <raki2001@o365.yonsei.ac.kr>
2020-11-23 02:03:01 +0900

    Updated to use VisualComponent

--- Runtime/RuntimeActionTemplates.cpp
+++ Runtime/RuntimeActionTemplates.cpp
@@ -3,26 +3,26 @@
 void RuntimeActionTemplates::MoveX(GameObject* target, float delta)
 {
 	target->position.x += delta;
-	target->sprite->setPositionX(target->sprite->getPositionX() + delta);
+	target->GetSprite()->setPositionX(target->GetSprite()->getPositionX() + delta);
 }
 
 void RuntimeActionTemplates::FlipSpriteXTrue(GameObject* target)
 {
-	target->sprite->setFlippedX(true);
+	target->GetSprite()->setFlippedX(true);
 }
 
 void RuntimeActionTemplates::FlipSpriteXFalse(GameObject* target)
 {
-	target->sprite->setFlippedX(false);
+	target->GetSprite()->setFlippedX(false);
 }
 
 void RuntimeActionTemplates::FlipSpriteX(GameObject* target)
 {
-	target->sprite->setFlippedX(target->sprite->isFlippedX());
+	target->GetSprite()->setFlippedX(target->GetSprite()->isFlippedX());
 }
 
 void RuntimeActionTemplates::JumpBy(GameObject* target, float duration, int height)
 {
 	auto action = JumpBy::create(duration, Vec2(0,0), height, 1);
-	target->sprite->runAction(action);
+	target->GetSprite()->runAction(action);
 }
