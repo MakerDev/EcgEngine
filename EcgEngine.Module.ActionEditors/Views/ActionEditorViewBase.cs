c2c262d10195833fbbafee4382390fc808956e60
yujinshin <raki2001@o365.yonsei.ac.kr>
2020-12-05 19:56:37 +0900

    Removed code duplication by adapting ActionEditorViewBase class

---
+++ EcgEngine.Module.ActionEditors/Views/ActionEditorViewBase.cs
@@ -0,0 +1,18 @@
+﻿using EcgEngine.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace EcgEngine.Module.ActionEditors.Views
+{
+    public class ActionEditorViewBase : UserControl, IActionEditorView
+    {
+        public Models.VisualScript.Action GetAction()
+        {
+            var vm = DataContext as IActionEditorViewModel;
+
+            return vm.GetAction();
+        }
+    }
+}
