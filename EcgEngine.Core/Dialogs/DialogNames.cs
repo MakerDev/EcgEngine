bde19284ab8ecd64e1e9375b4c3e0dd8f86f865c
yujinshin <raki2001@o365.yonsei.ac.kr>
2020-12-05 19:55:55 +0900

    Added textinputdialog

--- EcgEngine.Core/Dialogs/DialogNames.cs
+++ EcgEngine.Core/Dialogs/DialogNames.cs
@@ -7,5 +7,7 @@ namespace EcgEngine.Core.Dialogs
     public class DialogNames
     {
         public const string CONFIRMATION_DIALOG = "ConfirmationDialog";
+        public const string MESSAGE_DIALOG = "MessageDialog";
+        public const string TEXT_INPUT_DIALOG = "TextInputDialog";
     }
 }
---
+++ EcgEngine.Core/Dialogs/ViewModels/TextInputDialogViewModel.cs
@@ -0,0 +1,61 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcgEngine.Core.Dialogs.ViewModels
+{
+    public class TextInputDialogViewModel : BindableBase, IDialogAware
+    {
+        public string Title => "TextInputDialog";
+
+        public event Action<IDialogResult> RequestClose;
+
+
+        private string _message = "";
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+
+        private string _userInput = "";
+        public string UserInput
+        {
+            get { return _userInput; }
+            set { SetProperty(ref _userInput, value); }
+        }
+
+        public DelegateCommand CloseDialogCommand { get; set; }
+
+
+        public TextInputDialogViewModel()
+        {
+            CloseDialogCommand = new DelegateCommand(() =>
+            {
+                var dialogParam = new DialogParameters();
+                dialogParam.Add(nameof(UserInput), UserInput);
+
+                var dialogResult = new DialogResult(ButtonResult.OK, dialogParam);
+             
+                RequestClose?.Invoke(dialogResult);
+            });
+        }
+
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+            Message = parameters.GetValue<string>(nameof(Message));
+        }
+    }
+}
