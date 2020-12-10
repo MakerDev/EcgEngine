8468522a12062173687848ef6efc628c7ae4aea6
yujinshin <raki2001@o365.yonsei.ac.kr>
2020-12-11 01:57:06 +0900

    Added SelectedGameObjectChangedEvent

---
+++ EcgEngine.Core/Events/SelectedGameObjectChangedEvent.cs
@@ -0,0 +1,10 @@
+﻿using EcgEngine.Models;
+using Prism.Events;
+
+namespace EcgEngine.Core.Events
+{
+    public class SelectedGameObjectChangedEvent : PubSubEvent<GameObject>
+    {
+        public GameObject GameObject { get; set; } = null;
+    }
+}
