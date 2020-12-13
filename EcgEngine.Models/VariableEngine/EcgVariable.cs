3755473a16414991503e6ddc01b5b88e9558fd0e
yujinshin <raki2001@o365.yonsei.ac.kr>
2020-12-13 11:48:03 +0900

    Added ParentName property to EcgVariable

--- EcgEngine.Models/VariableEngine/EcgVariable.cs
+++ EcgEngine.Models/VariableEngine/EcgVariable.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -13,6 +14,7 @@ namespace EcgEngine.Models.VariableEngine
 
     public class EcgVariable
     {
+        public string ParentName { get; set; } = "";
         public string Name { get; set; }
         public EcgVariableType EcgVariableType { get; set; } = EcgVariableType.TypeString;
         public string Value { get; set; }
