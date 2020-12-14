83526fd37c7651dfb0f7d276b320cd1852d5169c
yujinshin <raki2001@o365.yonsei.ac.kr>
2020-12-14 22:42:45 +0900

    IncreaseVairabeEditor ver 1 added

---
+++ EcgEngine.Module.ActionEditors/Views/IncreaseVariableValueEditor.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace EcgEngine.Module.ActionEditors.Views
+{
+    /// <summary>
+    /// Interaction logic for IncreaseVariableValue
+    /// </summary>
+    public partial class IncreaseVariableValueEditor : ActionEditorViewBase
+    {
+        public IncreaseVariableValueEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
