c2f47d2597bdb785db0128c59f3f41c2463f095d
유진 신 <raki2001@naver.com>
2020-12-18 18:39:24 +0900

    Revert "Revert "demo with bug""
    This reverts commit fbea483bcad63dfe05df6dcb3505a099125503ec.

--- Runtime/MoveYFunctor.cpp
+++ Runtime/MoveYFunctor.cpp
@@ -20,19 +20,18 @@ void MoveYFunctor::Execute(float delta)
 	auto level = EngineManager::GetInstance()->GetDefaultLayer()->GetLevel();
 
 	tmp = level->positionToTileCoordinate(Point(player->getPositionX() + player_size.width * 0.5f,
-		player->getPositionY() + player_size.height * 0.5f ));
+		player->getPositionY() + player_size.height * 0.5f + _delta));
 
 	if (_delta > 0) {
 		tiles = level->getCollisionTilesY(tmp, 1);
 	}
 	else if (_delta < 0) {
 		tiles = level->getCollisionTilesY(tmp, -1);
-		//tiles = level->getCollisionTilesY(tmp, -1);
 	}
 
 	player_rect.setRect(
 		player->getBoundingBox().getMinX(),
-		player->getBoundingBox().getMinY() + _delta +1,
+		player->getBoundingBox().getMinY() + _delta,
 		player_size.width,
 		player_size.height
 	);
